d44a86274733c1a7781556d35de04badeaa3f08e
ellouzeali <ellouzeali1996@gmail.com>
2023-05-03 15:44:36 +0200

    Creating EC2 instance with terraform

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
-output "greeting" {
-  value = "Hello Ali !!"
+provider "aws" {
+  region = "us-east-1"
+  profile = "myprofile"
+}
+
+resource "aws_instance" "terraform_vm" {
+  ami = "ami-02396cdd13e9a1257"
+  subnet_id = "subnet-0619e6ccee9197e25"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my-first-tf-node"
+  }
+}
+output "message" {
+  value = "Creating EC2 instance with terraform!!"
 }
